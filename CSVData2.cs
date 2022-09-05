2a25f27efb16d52aa192463cd2bed8451a607c93
namratachavan24 <namrata2015.nc@gmail.com>
2022-09-06 00:09:54 +0530

    UC1 Added

---
+++ CSVData2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalyser
+{
+    public class CSVData2
+    {
+        public string State { get; set; }
+        public string Population { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IndianStatesCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StateCensusAnalyser obj = new StateCensusAnalyser();
+            obj.readCSV();
+        }
+    }
+}
