163cb6530cf8197d26a617fba3dcc7a54f2f18b4
Namrata Chavan <109748579+namratachavan24@users.noreply.github.com>
2022-09-06 15:22:12 +0530

    Test Case File Added

---
+++ Tests.cs
@@ -0,0 +1,86 @@
+using IndianStateCensusAnalyser;
+using IndianStateCensusAnalyser.Constructor_for_different_CSV;
+
+namespace IndianStateCensusAnalyserTest
+{
+    [TestClass]
+    public class Tests
+    {
+
+        
+        static string indiaStateCensusData = @"C:\Users\HP\RFP.NET\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\IndiaStateCensusData.csv";
+        static string wrongIndiaStateCensusData = @"C:\Users\HP\RFP.NET\IndianStatesCensusAnalyser\IndianStatesCensusAnalyserWrongIndiaStateCensusData.csv";
+        static string indiaStateCensusDataText = @"C:\Users\HP\RFP.NET\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\IndiaStateCensusDAta.txt";
+        static string delimiterIndiaStateCensusData = @"C:\Users\HP\RFP.NET\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\DelimiterIndiaStateCensusData.csv";
+
+
+        static string indianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
+
+
+        CensusAnalyser censusAnalyser;
+        Dictionary<string, CensusDTO> totalRecord;
+      
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            censusAnalyser = new CensusAnalyser();
+            totalRecord = new Dictionary<string, CensusDTO>();
+        }
+        
+        /// Test Case 1.1
+        
+        [TestMethod]
+        public void GivenIndianCensusDataFile_WhenReturnShouldReturnCensusDataCount()
+        {
+            //census Analyser Class is Called and parameters are passed like country, indian state census data which is csv file and header file.
+            //add
+            totalRecord = censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA, indiaStateCensusData, indianStateCensusHeaders);
+            //assert
+            //total no of rows excluding header are 29 in indian state census data.
+            Assert.AreEqual(29, totalRecord.Count);
+        }
+
+        /// test case 1.2
+        
+        [TestMethod]
+        public void GivenWrongIndianCensusDataFile_WhenReadedShouldReturnCustomException()
+        {            
+            var customException = Assert.ThrowsException<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA, wrongIndiaStateCensusData, indianStateCensusHeaders));
+           
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, customException.etype);
+        }
+        
+        /// test case 1.3
+       
+        [TestMethod]
+        public void GivenWrongIndianCensusDataType_WhenReadedShouldReturnCustomException()
+        {
+            
+            var customException = Assert.ThrowsException<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA, indiaStateCensusDataText, indianStateCensusHeaders));           
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE, customException.etype);
+        }
+
+        /// test case 1.4
+        
+        [TestMethod]
+        public void GivenIncorrectDelimiterForCensusData_WhenReadedShouldReturnCustomException()
+        {
+            
+            //add
+            var customException = Assert.ThrowsException<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA, delimiterIndiaStateCensusData, indianStateCensusHeaders));
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INCORRECT_DELIMITER, customException.etype);
+        }
+        
+        /// test case 1.5
+       
+        [TestMethod]
+        public void GivenIncorrectHeaderForCensusData_WhenReadedShouldReturnCustomException()
+        {
+           
+            var customException = Assert.ThrowsException<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA, wrongIndiaStateCensusData, indianStateCensusHeaders));
+            
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INCORRECT_HEADER, customException.etype);
+        }
+
+    }
+}
